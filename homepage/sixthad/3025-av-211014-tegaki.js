13c0e11b98bfffc5ade0f4cfd55db5520d3905f9
竹村瑠華 <takemura@takemuranoMacBook-Pro.local>
2021-10-21 12:54:56 +0900

    forgot pc

--- homepage/sixthad/3025-av-211014-tegaki.js
+++ homepage/sixthad/3025-av-211014-tegaki.js
@@ -1,4 +1,3 @@
-
 function unescapeHTML(str) {
   var div = document.createElement("div");
   div.innerHTML = str
@@ -9,7 +8,6 @@ function unescapeHTML(str) {
     .replace(/\n/g, "&#10;");
   return div.textContent || div.innerText;
 }
-
 
 document.body.style.width = "300";
 document.body.style.height = "250";
@@ -33,8 +31,9 @@ adArea_div.style.overflow = "hidden";
 adArea_div.style.border = "1.5px solid black";
 adArea_div.style.touchAction = "pan-y";
 adArea_div.style.zIndex = "1";
-adArea_div.style.background = "linear-gradient(0deg, black 3%, white 3%, white 5%, white 5%, white 5%, white 5%, white)";
-adArea_div.style.backgroundSize = "5px 30px";
+adArea_div.style.backgroundSize= "auto auto";
+adArea_div.style.backgroundColor= "rgba(255, 255, 255, 1)";
+adArea_div.style.backgroundImage= "repeating-linear-gradient(178deg, transparent, transparent 41px, rgba(207, 207, 207, 1) 41px, rgba(207, 207, 207, 1) 42px )";
 adArea_div.style.fontFamily = "ヒラギノ丸ゴ Pro W4", "ヒラギノ丸ゴ Pro", "Hiragino Maru Gothic Pro", "ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", "HG丸ｺﾞｼｯｸM-PRO", "HGMaruGothicMPRO";
 document.body.appendChild(adArea_div);
 var swiperWrapper = document.createElement('div');
@@ -49,7 +48,7 @@ swiperWrapper.style.boxSizing = "content-box";
 adArea_div.appendChild(swiperWrapper);
 var swiperButtonNext = document.createElement('img');
 swiperButtonNext.className = "buttonNext";
-swiperButtonNext.src = 'arrow-min.png';
+swiperButtonNext.src = 'https://cnobi.jp/v1/dragon/dyn-banner/dmm/211011arrow.png';
 swiperButtonNext.style.zIndex = '2';
 swiperButtonNext.style.top = '70px';
 swiperButtonNext.style.right = '0px';
@@ -59,7 +58,7 @@ swiperButtonNext.style.cursor = "pointer";
 adArea_div.appendChild(swiperButtonNext);
 var swiperButtonPrev = document.createElement('img');
 swiperButtonPrev.className = "buttonPrev";
-swiperButtonPrev.src = 'arrow-min.png';
+swiperButtonPrev.src = 'https://cnobi.jp/v1/dragon/dyn-banner/dmm/211011arrow.png';
 swiperButtonPrev.style.zIndex = '2';
 swiperButtonPrev.style.top = '70px';
 swiperButtonPrev.style.left = '0px';
@@ -110,7 +109,6 @@ ninja_ad_items.slice(1, 5).forEach((item) => {
 	div_textarea.style.borderStyle = "solid";
 	div_textarea.style.borderWidth = "1.5px";
 	div_textarea.style.cursor = "pointer";
-	div_textarea.style.display = "inline - block";
 	div_textarea.style.fontSize = "1rem";
 	div_textarea.style.outline = "none";
 	div_textarea.style.padding = "0.75rem";
@@ -123,23 +121,60 @@ ninja_ad_items.slice(1, 5).forEach((item) => {
 	div_textarea.style.width = "272px";
 	div_textarea.style.height = "52px";
 	div_textarea.style.padding = "0 5px";
+	div_textarea.style.zIndex = "1";
 	slide_a.appendChild(div_textarea);
+
+	var simpleTextarea = document.createElement('div');
+	simpleTextarea.className  = 'textArea';
+	simpleTextarea.style.display  = 'inline';
+	simpleTextarea.style.overflow = 'hidden';
+	simpleTextarea.style.height = "50px";
+	div_textarea.appendChild(simpleTextarea);
+
 	var p_textarea = document.createElement('p');
-	p_textarea.style.position = "absolute";
-	p_textarea.style.paddingLeft = "20px";
-	p_textarea.style.paddingRight = "20px";
-	p_textarea.style.textAlign = "center";
+	p_textarea.className = 'text';
+	p_textarea.style.position = "relative";
 	p_textarea.style.color = "#4285F4";
 	p_textarea.style.fontSize = "13px";
-	p_textarea.style.margin = "0px";
+	p_textarea.style.margin = "10px";
 	p_textarea.style.overflow = "hidden";
 	p_textarea.style.textOverflow = "ellipsis";
+	p_textarea.style.display = "block";
 	p_textarea.style.display = "-webkit-box";
 	p_textarea.style.webkitLineClamp = "2";
+	p_textarea.style.textAlign = "center";
+	p_textarea.style.verticalAlign= "center";
+	p_textarea.style.right = "0px";
+	p_textarea.style.left = "0px";
+	p_textarea.style.padding = "0";
+	p_textarea.style.zIndex = "3";
 	p_textarea.innerHTML = item.title;
-	div_textarea.appendChild(p_textarea);
+	simpleTextarea.appendChild(p_textarea);
 	class_num++;
 })
+
+function chk(i)
+{
+	var textElement = document.getElementsByClassName('text').item(i);
+	var textArea = document.getElementsByClassName('textArea').item(i);
+	var height = getComputedStyle(textElement).height;
+
+	if (height == '20px')
+	{
+		textElement.style.bottom = '-3.5px';
+	}
+	else
+	{
+		textElement.style.bottom = '4px';
+		textElement.style.height = '40px';
+	}
+
+}
+chk(0);
+chk(1);
+chk(2);
+chk(3);
+
 var slideChanger = function () {
 }
 let classIndex = 1;
